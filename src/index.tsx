6c07fba423be86c31cdaef8b1c7cb572b1ac8855
Treille Léonard <leonard.treille@gmail.com>
2023-04-30 17:37:31 +0200

    chore: Add pwa related files

--- src/index.tsx
+++ src/index.tsx
@@ -28,3 +28,7 @@ const documentHeight = () => {
 };
 window.addEventListener("resize", documentHeight);
 documentHeight();
+
+if (!import.meta.env.DEV && "serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js");
+}
